dc986f706bdf22f4740f48bda9ac4999b7558905
Ricardo Santos <morpheu_jose@hotmail.com>
2023-05-09 15:10:35 -0300

    FINAL MODULO 10

--- src/test/java/com/algafoods/KitchenRegistrationIT.java
+++ src/test/java/com/algafoods/KitchenRegistrationIT.java
@@ -1,70 +1,152 @@
 package com.algafoods;
 
-import com.algafoods.domain.exception.EntityInUseException;
-import com.algafoods.domain.exception.EntityNotFoundException;
+import com.algafoods.api.controller.KitchenController;
 import com.algafoods.domain.model.KitchenModel;
-import com.algafoods.domain.service.KitchenService;
-import org.junit.jupiter.api.Assertions;
+import com.algafoods.util.DatabaseCleaner;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.TransactionSystemException;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@TestPropertySource("/application-test.properties")
 class KitchenRegistrationIT {
 
+    @LocalServerPort
+    private int port;
     @Autowired
-    private KitchenService kitchenService;
+    private DatabaseCleaner databaseCleaner;
+    @BeforeEach
+    void setUp(){
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/kitchens";
+
+        databaseCleaner.clearTables();
+        prepareData();
+    }
+    @Autowired
+    private KitchenController kitchenController;
+
+//    @Test
+//    void mustSucceed_WhenKitchenRegistration(){
+//        //cenario
+//        KitchenModel newKitchen  = new KitchenModel();
+//        newKitchen.setName("Brasileira");
+//        //acao
+//        newKitchen = kitchenService.save(newKitchen);
+//        //validacao
+//        assertThat(newKitchen).isNotNull();
+//        assertThat(newKitchen.getId()).isNotNull();
+//    }
+//
+//    @Test
+//    void mustFail_WhenKitchenNameIsNull(){
+//        //Cenario
+//        KitchenModel newKitchen =  new KitchenModel();
+//        newKitchen.setName(null);
+//
+//        Throwable error =
+//                    Assertions.assertThrows(TransactionSystemException.class,
+//                            () -> kitchenService.save(newKitchen));
+//
+//            assertThat(error).isNotNull();
+//    }
+//
+//    @Test
+//    void mustFail_WhenDeleteKitchenInUse(){
+//        var newKitchen = new KitchenModel();
+//        newKitchen.setId(UUID.fromString("8ebd29bc-0946-4823-aeda-42a01b5210e8"));
+//
+//        Throwable error
+//                = Assertions.assertThrows(EntityInUseException.class,
+//                () -> kitchenService.delete(newKitchen));
+//
+//        assertThat(error).isNotNull();
+//    }
+//
+//    @Test
+//    void mustFail_WhenKitchenNotFound(){
+//        Throwable error
+//                = Assertions.assertThrows(EntityNotFoundException.class,
+//                () -> kitchenController.delete(UUID.fromString("8ebd29bc-0946-4823-aeda-42a01b5210e7")));
+//
+//        assertThat(error).isNotNull();
+//    }
+
     @Test
-    void mustSucceed_WhenKitchenRegistration(){
-        //cenario
-        KitchenModel newKitchen  = new KitchenModel();
-        newKitchen.setName("Brasileira");
-        //acao
-        newKitchen = kitchenService.save(newKitchen);
-        //validacao
-        assertThat(newKitchen).isNotNull();
-        assertThat(newKitchen.getId()).isNotNull();
+    void mustStatus200Return_WhenKitchenFindAll(){
+        RestAssured.given()
+                    .accept(ContentType.JSON)
+                .when()
+                    .get()
+                .then()
+                    .statusCode(HttpStatus.OK.value());
+    }
+    @Test
+    void mustHave2Kitchens_WhenKitchenFindAll(){
+        RestAssured.given()
+                    .accept(ContentType.JSON)
+                .when()
+                    .get()
+                .then()
+                    .body("", Matchers.hasSize(2));
+                    //.body("name", Matchers.hasItems("Arabe", "Indiana"));
     }
 
     @Test
-    void mustFail_WhenKitchenNameIsNull(){
-        //Cenario
-        KitchenModel newKitchen =  new KitchenModel();
-        newKitchen.setName(null);
-
-        Throwable error =
-                    Assertions.assertThrows(TransactionSystemException.class,
-                            () -> kitchenService.save(newKitchen));
-
-            assertThat(error).isNotNull();
+    void mustStatus201Return_WhenKitchenRegistration(){
+        RestAssured.given()
+                    .body("{\"name\": \"Indiana\"}")
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                .when()
+                    .post()
+                .then()
+                    .statusCode(HttpStatus.CREATED.value());
     }
 
     @Test
-    void mustFail_WhenDeleteKitchenInUse(){
-        var newKitchen = new KitchenModel();
-        newKitchen.setId(UUID.fromString("8ebd29bc-0946-4823-aeda-42a01b5210e8"));
-
-        Throwable error
-                = Assertions.assertThrows(EntityInUseException.class,
-                () -> kitchenService.delete(newKitchen));
-
-        assertThat(error).isNotNull();
+    void mustReturnRequestAndStatusCorrect_WhenFindByIdKitchen(){
+        RestAssured.given()
+                    .pathParams("id", UUID.fromString("8ebd29bc-0946-4823-aeda-42a01b5210e8"))
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                .when()
+                    .get("/{id}")
+                .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .body("name", equalTo("Arabe"));
+    }
+    @Test
+    void mustReturnStatus404_WhenFindByIdKitchenNotFound(){
+        RestAssured.given()
+                    .pathParams("id", UUID.fromString("8ebd29bc-0946-4823-aeda-42a01b5210e7"))
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                .when()
+                    .get("/{id}")
+                .then()
+                    .statusCode(HttpStatus.NOT_FOUND.value());
     }
 
-    @Test
-    void mustFail_WhenKitchenNotFound(){
-        var newKitchen = new KitchenModel();
-        newKitchen.setId(UUID.fromString("8ebd29bc-0946-4823-aeda-42a01b5210e7"));
 
-        Throwable error
-                = Assertions.assertThrows(EntityNotFoundException.class,
-                () -> kitchenService.delete(newKitchen));
+    private void prepareData(){
+        KitchenModel kitchenModel1 = new KitchenModel();
+        kitchenModel1.setName("Tailandesa");
+        kitchenController.save(kitchenModel1);
 
-        assertThat(error).isNotNull();
+        KitchenModel kitchenModel2 = new KitchenModel();
+        kitchenModel2.setName("Americana");
+        kitchenController.save(kitchenModel2);
     }
 }
